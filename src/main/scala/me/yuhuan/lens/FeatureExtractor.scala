440a818022bce1bc45b118751f16516a9e0f56cb
yuhuan <jyuhuan@gmail.com>
2015-11-11 16:15:57 -0500

    +

--- src/main/scala/me/yuhuan/lens/FeatureExtractor.scala
+++ src/main/scala/me/yuhuan/lens/FeatureExtractor.scala
@@ -3,8 +3,25 @@ package me.yuhuan.lens
 /**
   * @author Yuhuan Jiang (jyuhuan@gmail.com).
   */
-trait FeatureExtractor[-X] {
-  def featurizers: Iterable[Featurizer[X, Any]]
+
+trait SimpleFeatureExtractor[-X] { outer ⇒
+  def apply(x: X): FeatureVector
+
+  def ++[Y <: X](that: SimpleFeatureExtractor[Y]) = new SimpleFeatureExtractor[Y] {
+    override def apply(y: Y): FeatureVector = {
+      outer(y) ++ that(y)
+    }
+  }
+}
+
+object SimpleFeatureExtractor {
+  def apply[X](fxs: SimpleFeatureExtractor[X]*) = new SimpleFeatureExtractor[X] {
+    def apply(x: X): FeatureVector = fxs.reduce(_ ++ _)(x)
+  }
+}
+
+trait FeatureExtractor[-X] extends SimpleFeatureExtractor[X] { outer ⇒
+  val featurizers: Iterable[Featurizer[X, Any]]
 
   def apply(x: X): FeatureVector = {
     val features = featurizers.flatMap(f ⇒ f(x).values.map { case (v, a) ⇒ Feature(f.name, v, a) })
@@ -14,6 +31,6 @@ trait FeatureExtractor[-X] {
 
 object FeatureExtractor {
   def apply[X](fs: Featurizer[X, Any]*) = new FeatureExtractor[X] {
-    def featurizers: Iterable[Featurizer[X, Any]] = fs
+    val featurizers: Iterable[Featurizer[X, Any]] = fs
   }
 }
